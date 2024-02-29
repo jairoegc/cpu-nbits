1cb9a2a33906977f4e187ab663b098aeb03c31bf
jairoegc <jairoegc@gmail.com>
2024-02-29 11:28:15 -0300

    memory module using saed32 sram IP macro

--- rtl/memory.v
+++ rtl/memory.v
@@ -18,29 +18,29 @@ module memory #(
     output logic [2*WIDTH-1:0] memoryOutData
 );
 
-/*SRAMLP2RW128x16 inst_SRAM1(
-    .A1(),      # 7bits address
-    .CE1(),     # CLK
-    .CSB1(),    # enable ram1
-    .WE1(),     # Write enable 1
-    .OEB1(),    # Read enable 1
-    .I1(),      # 16bits
-    .O1(),      # 16bits
-    .LS1(),     #
-    .DS1(),     #
-    .A2(),      # 7bits adress
-    .CE2(),     # CLK
-    .CSB2(),    # enable ram2
-    .WE2(),     # Write enable 2
-    .OEB2(),    # Read enable 2
-    .I2(),      # 16bits
-    .O2(),      # 16bits
-    .LS2(),     #
-    .DS2(),     #
-    .SD()       #
+SRAMLP2RW128x16 inst_SRAM1(
+    .A1(memoryAddress[6:0]),    // 7bits address
+    .CE1(clk),                  // CLK
+    .CSB1(~memoryAddress[7]),   // enable ram1
+    .WE1(memoryWrite),          // Write enable 1
+    .OEB1(memoryRead),          // Read enable 1
+    .I1(memoryWriteData),       // 16bits
+    .O1(memoryOutData),         // 16bits
+    .LS1(1'd0),
+    .DS1(1'd0),
+    .A2(memoryAddress[6:0]),    // 7bits adress
+    .CE2(clk),                  // CLK
+    .CSB2(memoryAddress[7]),    // enable ram2
+    .WE2(memoryWrite),          // Write enable 2
+    .OEB2(memoryRead),          // Read enable 2
+    .I2(memoryWriteData),       // 16bits
+    .O2(memoryOutData),         // 16bits
+    .LS2(1'd0),
+    .DS2(1'd0),
+    .SD(1'd0)
 );
-*/
 
+/*
 logic [2*WIDTH:0] mem [255:0];
 
 assign memoryOutData = (memoryRead && !memoryWrite)? mem[memoryAddress]:0;
@@ -60,6 +60,6 @@ always_ff@(posedge clk)
 begin
     mem[memoryAddress] <= mem_next;
 end
-
+*/
 
 endmodule
