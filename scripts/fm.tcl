5213f78079ae935b2419d360bcbcd8e75df39c8d
jairoegc <jairoegc@gmail.com>
2024-03-04 11:29:20 -0300

    Formality script updated

--- scripts/fm.tcl
+++ scripts/fm.tcl
@@ -7,7 +7,7 @@ set library saed32
 
 ##### FM #######
 
-set_app_var search_path "$search_path . ./rtl ./libs/DBs ./my_run"
+set_app_var search_path "$search_path . ./rtl ./libs/DBs ./outputs"
 
 #fm_shell -f ./scripts/fm.tcl | & tee -i ./logs/fm.log
 
@@ -27,7 +27,7 @@ set_top ${module_name}_WIDTH${n-bits}
 
 report_guidance > reports/guidance_${module_name}${n-bits}bits_${library}.rpt
 
-save_session -replace ./my_run/pre_verify_${module_name}${n-bits}bits_${library}.fss 
+save_session -replace ./sessions/pre_verify_${module_name}${n-bits}bits_${library}.fss 
 
 match > reports/match_${module_name}${n-bits}bits_${library}.rpt
 
@@ -38,6 +38,6 @@ verify > reports/verify_${module_name}${n-bits}bits_${library}.rpt
 report_verify_points > reports/verify_points_${module_name}${n-bits}bits_${library}.rpt
 report_analysis_results -summary > reports/analysis_summary_${module_name}${n-bits}bits_${library}.rpt
 
-save_session -replace ./my_run/post_verify_${module_name}${n-bits}bits_${library}.fss 
+save_session -replace ./sessions/post_verify_${module_name}${n-bits}bits_${library}.fss 
 
 exit 
